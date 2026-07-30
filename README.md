977c8a02b3dec495d4b1b5bc9e9b1907b7a1d415
Kim Audrey Magan <work.kimmagan@gmail.com>
2026-07-31 01:12:01 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# dep-submission-pipeline
+# DEP Open Track Submission Pipeline
+This year the Data Engineering Pilipinas has held its Open Track Program where they have accepted 50 builders to learn data engineering. The tracking of the builders, however, poses a challenge where it is difficult to see where milestones a builder is in.
+
+### Grain: The state of the submission of a builder at a specific point in time.
---
+++ src/etl/dimenstions.py
@@ -0,0 +1,39 @@
+"""
+dim_issue
+    - id
+    - issue_id
+    - title
+    - url
+    - author
+
+dim_author
+    - id
+    - name
+    - github_username
+    - email
+
+dim_date
+    - date
+
+dim_milestone
+    - id
+    - name
+    - description
+    - date
+
+dim_status
+    - id
+    - name
+    - description
+
+dim_reviewer
+    - id
+    - github_id
+    - username
+    - name
+
+dim_state
+    - id
+    - name
+    - description
+"""
